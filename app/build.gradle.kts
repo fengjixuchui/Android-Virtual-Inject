e67a9f06da3bda8b982ac6f81da169fa57c545fd
Reveny <113244907+reveny@users.noreply.github.com>
2025-01-21 12:12:55 +0100

    Update to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.reveny.virtualinject"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
